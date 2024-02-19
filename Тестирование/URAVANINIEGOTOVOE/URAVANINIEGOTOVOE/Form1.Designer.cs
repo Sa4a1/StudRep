1b3338447b0f6deaac753b2ef18b5902e7afada1
Arsen Iskhakov <ar.eec@mail.ru>
2024-02-20 02:09:29 +0500

    asd

---
+++ Тестирование/URAVANINIEGOTOVOE/URAVANINIEGOTOVOE/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace URAVANINIEGOTOVOE
+{
+    partial class Ura
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 146);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(344, 146);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(579, 146);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(334, 228);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 34);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "результат";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(138, 72);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(76, 34);
+            this.button5.TabIndex = 4;
+            this.button5.Text = ",";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(417, 72);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 34);
+            this.button6.TabIndex = 5;
+            this.button6.Text = ",";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(658, 73);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 34);
+            this.button7.TabIndex = 6;
+            this.button7.Text = ",";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(233, 336);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 19);
+            this.label1.TabIndex = 7;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(18, 77);
+            this.txtA.Margin = new System.Windows.Forms.Padding(4);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(104, 26);
+            this.txtA.TabIndex = 8;
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtA_KeyPress);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(286, 76);
+            this.txtB.Margin = new System.Windows.Forms.Padding(4);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(104, 26);
+            this.txtB.TabIndex = 9;
+            this.txtB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtB_KeyPress);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(543, 77);
+            this.txtC.Margin = new System.Windows.Forms.Padding(4);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(104, 26);
+            this.txtC.TabIndex = 10;
+            this.txtC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtC_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 19);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Коэффициент а";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Коэффициент b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(534, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 19);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Коэффициент c";
+            // 
+            // Ura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(820, 420);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Ura";
+            this.Text = "Iskhakov Uranenie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Тестирование/URAVANINIEGOTOVOE/URAVANINIEGOTOVOE/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace URAVANINIEGOTOVOE
+{
+    public partial class Ura : Form
+    {
+        public static int CountA = 0;
+        public static int CountB = 0;
+        public static int CountC = 0;
+        public static int PosA = 0;
+        public static int PosB = 0;
+        public static int PosC = 0;
+        
+        public Ura()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (txtA.Text != "")
+            {
+                string substr = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0].ToString();
+                int count = (txtA.Text.Length - txtA.Text.Replace(substr, "").Length) / substr.Length;
+                if (count == 0) txtA.Text = txtA.Text + ",";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (txtB.Text != "")
+            {
+                string substr = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0].ToString();
+                int count = (txtB.Text.Length - txtB.Text.Replace(substr, "").Length) / substr.Length;
+                if (count == 0) txtB.Text = txtB.Text + ",";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (txtC.Text != "")
+            {
+                string substr = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0].ToString();
+                int count = (txtC.Text.Length - txtC.Text.Replace(substr, "").Length) / substr.Length;
+                if (count == 0) txtC.Text = txtC.Text + ",";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtA.Clear();
+            CountA = 0;
+            PosA = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtB.Clear();
+            CountB = 0;
+            PosB = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtC.Clear();
+            CountC = 0;
+            PosC = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StringBuilder error = new StringBuilder();
+
+            if (txtA.Text == "-") error.AppendLine("Введите еще число в коэффициент а!");
+            if (txtB.Text == "-") error.AppendLine("Введите еще число в коэффициент b!");
+            if (txtC.Text == "-") error.AppendLine("Введите еще число в коэффициент c!");
+            if (string.IsNullOrWhiteSpace(txtA.Text)) error.AppendLine("Введите коэфициент А");
+            if (string.IsNullOrWhiteSpace(txtB.Text)) error.AppendLine("Введите коэфициент B");
+            if (string.IsNullOrWhiteSpace(txtC.Text)) error.AppendLine("Введите коэфициент C");
+            
+            if (error.Length > 0)
+            {
+                MessageBox.Show(error.ToString());
+                return;
+            }
+
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+            double c = Convert.ToDouble(txtC.Text);
+            if (a > 50 || a < -50) error.AppendLine("Коэффициент а вне диапазона допустимых значений!");
+            if (b > 50 || b < -50) error.AppendLine("Коэффициент b вне диапазона допустимых значений!");
+            if (c > 50 || c < -50) error.AppendLine("Коэффициент c вне диапазона допустимых значений!");
+          
+
+            if (error.Length > 0)
+            {
+                MessageBox.Show(error.ToString());
+                return;
+            }
+            else
+            {
+               
+
+                if (a == 0) label1.Text = "Уравнение не квадратное, так как а = 0";
+                else
+                {
+                    double D = Math.Pow(b, 2) - (4 * a * c);
+                    if (D < 0) label1.Text = "Корней нет";
+                    else if (D == 0)
+                    {
+                        double x = (-b) / (2 * a);
+                        label1.Text = $"x = {x}";
+                    }
+                    else
+                    {
+                        double x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                        double x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                        label1.Text = $"x1 = {x1} x2 = {x2}";
+                    }
+                }
+            }
+        }
+
+        private void txtB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+         
+            String txtb = txtB.ToString();
+
+            if (PosB == 0 && CountB == 0 && e.KeyChar == '-')
+            {
+                CountB++;
+                PosB++;
+            }
+            else if (PosB >= 0 && CountB >= 0)
+            {
+                e.Handled = !(Char.IsDigit(e.KeyChar));
+                PosB++;
+                CountB++;
+            }
+
+    }
+
+        private void txtA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (CountA == 0 && e.KeyChar == '-' && PosA == 0)
+            {
+                CountA ++;
+                PosA++;
+            }
+            else if (PosA >= 0 && CountA >= 0)
+            {
+                e.Handled = !(Char.IsDigit(e.KeyChar));
+                PosA++;
+                CountA++;
+               
+            }
+
+        }
+
+        private void txtC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (CountC == 0 && e.KeyChar == '-' && PosC == 0)
+            {
+                CountC++;
+                PosC++;
+            }
+            else if (PosC >= 0 && CountC >=0)
+            {
+                e.Handled = !(Char.IsDigit(e.KeyChar));
+                PosC++;
+                CountC++;
+            }
+
+        }
+    }
+ }
